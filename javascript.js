88542fca2206468426bfdc386faa78979636c774
gerteck <changerteck@gmail.com>
2023-05-07 11:41:08 +0800

    Added UI elements

--- javascript.js
+++ javascript.js
@@ -1,13 +1,26 @@
 console.log("Hello There!")
 
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+
+
 function capitalize(string) {
     const str = string.charAt(0).toUpperCase() + string.slice(1);
     return str;
 }
 
-const ROCK = 0;
-const PAPER = 1;
-const SCISSORS = 2;
+const rock = document.querySelector('button.rock');
+const paper = document.querySelector('button.paper');
+const scissors = document.querySelector('button.scissors');
+
+
+console.log(rock);
+
+rock.addEventListener('click', () => playRound("rock", getComputerChoice()));
+paper.addEventListener('click', () => playRound("paper", getComputerChoice()));
+scissors.addEventListener('click', () => playRound("scissors", getComputerChoice()));
+
 
 // Returns string
 function getComputerChoice() {
@@ -24,40 +37,46 @@ function getComputerChoice() {
     }
 }
 
+const results = document.querySelector('.results');
+
+
+
 function playRound(playerSelection, computerSelection) {
     let player = capitalize(playerSelection.toLowerCase());
     let computer = capitalize(computerSelection.toLowerCase());
+    console.log("played!");
+    let string = "";
 
-    //Rock
     if (computer == player) {
-        return "Tie! " + player + " ties with " + computer; 
+        string = "Tie! " + player + " ties with " + computer; 
     
     } else if (player == "Rock") {
         if (computer == "Scissors") {
-            return "You Win! " + player + " beats " + computer; 
+            string = "You Win! " + player + " beats " + computer; 
         }
         else {
-            return "You Lose! " + computer + " beats " + player;
+            string = "You Lose! " + computer + " beats " + player;
         }
     
     } else if (player == "Paper") {
         if (computer == "Rock") {
-            return "You Win! " + player + " beats " + computer; 
+            string = "You Win! " + player + " beats " + computer; 
         }
         else {
-            return "You Lose! " + computer + " beats " + player;
+            string = "You Lose! " + computer + " beats " + player;
         }
     
     } else if (player == "Scissors") {
         if (computer == "Paper") {
-            return "You Win! " + player + " beats " + computer; 
+            string = "You Win! " + player + " beats " + computer; 
         }
         else {
-            return "You Lose! " + computer + " beats " + player;
+            string = "You Lose! " + computer + " beats " + player;
         }
     }
-
-    return "If not error."
+    
+    results.textContent = string;
+    // return string;
 }
 
 // Plays RPS 5 times
@@ -69,7 +88,7 @@ function game() {
     }
 }
 
-game();
+//game();
 
 
 
